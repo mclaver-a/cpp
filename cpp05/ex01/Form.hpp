95b840fa0cec1bba3bfb90d0da9b84359c54b5c5
Max Claver Aubry <maxclau2002@gmail.com>
2025-10-09 12:10:27 +0200

    Update and rename cpp05/Form.hpp to cpp05/ex01/Form.hpp

---
+++ cpp05/ex01/Form.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+class Form
+{
+  private:
+    const std::string _name;
+    const bool        _signed;
+    const int         _gradeToSign;
+    const int         _gradeToExecute;
+  public:
+    
+
+
+}
