f61d451736250ce55bb38d1d917eaed9238d41ec
Max Claver Aubry <maxclau2002@gmail.com>
2025-10-09 09:54:24 +0200

    Create Bureaucrat.hpp

---
+++ Bureaucrat.hpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <iostream>
+
+#define MAX_GRADE 1
+#define MIN_GRADE 150
+
+class  Bureaucrat
+{
+  private:
+    const std::string  _name;
+    int                _grade;
+  public:
+    Bureaucrat(),
+    Bureaucrat(std::string name);
+    Bureaucrat(std::string name, int grade);
+    ~Bureaucrat();
+
+    Bureaucrat & operator = (const Bureaucrat &src);
+    
+    std::string  getName(void) const;
+    int          getGrade(void) const;
+
+    class  GradeTooHighException : public std::exception
+    {
+      public:
+        const char *what(void) const throw();
+    };
+    class  GradeTooLowException : public std::exception
+    {
+      public:
+        const char *what(void) const throw();
+    };
+};
+
+std::ostream & operator << (std::ostream &out, Bureaucrat brc);
