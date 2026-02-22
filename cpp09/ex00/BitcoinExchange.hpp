208d06f621971bf5fcfdbd0e4f2967263d6328a5
Max Claver Aubry <maxclau2002@gmail.com>
2026-02-22 19:47:13 +0100

    Add BitcoinExchange class header file

---
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,27 @@
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+
+class BitcoinExchange {
+private:
+    std::map<std::string, float> _data;
+    bool _isValidDate(const std::string& date);
+    bool _isValidValue(const std::string& str, float& value);
+
+public:
+    BitcoinExchange();
+    BitcoinExchange(const std::string& dbPath);
+    BitcoinExchange(const BitcoinExchange& other);
+    BitcoinExchange& operator=(const BitcoinExchange& other);
+    ~BitcoinExchange();
+
+    void processInput(const std::string& inputPath);
+};
+
+#endif
