4ac3fc0b708f2e31233f4f0330cec9d6f8d6cf1c
Max Claver Aubry <maxclau2002@gmail.com>
2026-02-23 23:26:06 +0100

    Refactor PmergeMe class and add new methods

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -4,21 +4,36 @@
 #include <iostream>
 #include <vector>
 #include <deque>
+#include <string>
 #include <ctime>
 #include <algorithm>
-#include <iterator>
+#include <cstdlib>
+#include <climits>
 
 class PmergeMe {
+private:
+    std::vector<int> _vec;
+    std::deque<int> _deq;
+
+    // Vector Implementation
+    void sortVector(std::vector<int>& container);
+    std::vector<int> fordJohnsonVec(std::vector<int>& container);
+    
+    // Deque Implementation
+    void sortDeque(std::deque<int>& container);
+    std::deque<int> fordJohnsonDeq(std::deque<int>& container);
+
+    // Helpers
+    bool isAllPosInt(int ac, char **av);
+    std::vector<size_t> generateJacobsthal(size_t n);
+
 public:
     PmergeMe();
+    PmergeMe(const PmergeMe& other);
+    PmergeMe& operator=(const PmergeMe& other);
     ~PmergeMe();
-    void run(int ac, char **av);
 
-private:
-    void sortVector(std::vector<int>& v);
-    void sortDeque(std::deque<int>& d);
-    template <typename T>
-    void fordJohnson(T& container);
+    void execute(int ac, char **av);
 };
 
 #endif
